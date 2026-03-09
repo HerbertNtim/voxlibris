466aca87ff476baeff3e1c112e0ed3d5b18c8967
HerbertNtim <herbertntim15@gmail.com>
2026-03-09 15:59:11 +0000

    working on hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import HeroSection from '@/components/HeroSection';
+
 const Home = () => {
-  return <div className="">Hi</div>;
+  return (
+    <main className="wrapper- container">
+      <HeroSection />
+    </main>
+  );
 };
 
 export default Home;
