6194bf669bac619e473d341caadb4b8210ac607a
HerbertNtim <herbertntim15@gmail.com>
2026-03-09 17:29:41 +0000

    building home ui

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
 const HeroSection = () => {
-  return <div>HeroSection</div>;
+  return (
+    <section className="wrapper mb:10 md:mb-16">
+      <div className="library-hero-card">
+        <div className="library-hero-content">
+          {/* Left Part */}
+          <div></div>
+
+          {/* Center part - Desktop*/}
+          <div></div>
+
+          {/* Center part - Mobile (Hidden on Desktop) */}
+          <div></div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default HeroSection;
