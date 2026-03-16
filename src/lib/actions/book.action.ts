67b7a0065b98a5dce0af42fdf34439502da7c066
HerbertNtim <herbertntim15@gmail.com>
2026-03-16 13:04:53 +0000

    function to save book segments

--- src/lib/actions/book.action.ts
+++ src/lib/actions/book.action.ts
@@ -1,7 +1,8 @@
 import { connectToDatabase } from '@/database/mongoose';
-import { CreateBook } from 'types';
+import { CreateBook, TextSegment } from 'types';
 import { generateSlug, serializeData } from '../utils';
 import Book from '@/database/models/book.model';
+import BookSegment from '@/database/models/book-segment.model';
 
 export const createBook = async (data: CreateBook) => {
   try {
@@ -21,5 +22,44 @@ export const createBook = async (data: CreateBook) => {
   } catch (e) {
     console.error('Error creating book:', e);
     return { success: false, error: e };
+  }
+};
+
+export const saveBookSegments = async (
+  bookId: string,
+  clerkId: string,
+  segments: TextSegment[],
+) => {
+  try {
+    await connectToDatabase();
+
+    const segmentsToInsert = segments.map(
+      ({ text, segmentIndex, pageNumber, wordCount }) => ({
+        bookId,
+        clerkId,
+        text,
+        segmentIndex,
+        pageNumber,
+        wordCount,
+      }),
+    );
+
+    await BookSegment.insertMany(segmentsToInsert);
+    await Book.findByIdAndUpdate(bookId, {
+      totalSegments: segmentsToInsert.length,
+    });
+
+    console.log(`Saved ${segmentsToInsert.length} segments for book ${bookId}`);
+
+    return { success: true, data: { segmentCreated: segments.length } };
+  } catch (error) {
+    console.error('Error saving book segments:', error);
+
+    await BookSegment.deleteMany({ bookId });
+    await Book.findByIdAndDelete(bookId);
+
+    console.log('Rolled back book and segments due to error.');
+
+    return { success: false, error };
   }
 };
