38f58a6cd2cbb2907b6b857ba58dbfa397e0d0e0
HerbertNtim <herbertntim15@gmail.com>
2026-03-20 07:08:36 +0000

    fixing img browser error

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,7 +19,13 @@ const Navbar = () => {
     <header className="w-full fixed z-50 bg-(--bg-primary)">
       <div className="wrapper navbar-height py-4 flex justify-between items-center">
         <Link href="/" className="flex items-center gap-0.5">
-          <Image src="/logo.png" alt="VoxLibris" width={42} height={26} />
+          <Image
+            src="/logo.png"
+            alt="VoxLibris"
+            width={42}
+            height={26}
+            style={{ width: 42, height: 26 }}
+          />
           <span className="logo-text">VoxLibris</span>
         </Link>
 
