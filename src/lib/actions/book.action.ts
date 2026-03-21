5d0dc686081bb268ef0c2a366a41e980b724742b
HerbertNtim <herbertntim15@gmail.com>
2026-03-21 03:49:59 +0000

    get all books function

--- src/lib/actions/book.action.ts
+++ src/lib/actions/book.action.ts
@@ -6,6 +6,18 @@ import { generateSlug, serializeData } from '../utils';
 import Book from '@/database/models/book.model';
 import BookSegment from '@/database/models/book-segment.model';
 
+export const getAllBooks = async () => {
+  try {
+    await connectToDatabase();
+
+    const books = await Book.find().sort({ createdAt: -1 }).lean();
+    return { success: true, data: serializeData(books) };
+  } catch (e) {
+    console.error('Error fetching books:', e);
+    return { success: false, error: e };
+  }
+};
+
 export const checkBookExists = async (title: string) => {
   try {
     await connectToDatabase();
